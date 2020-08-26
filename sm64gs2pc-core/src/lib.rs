0ae1bb4eab76e806f9a0dcefe0a43439150c143e
Benjamin Levy <blevy@protonmail.com>
2020-08-25 22:30:42 -0400

    Remove unneeded exports

--- sm64gs2pc-core/src/lib.rs
+++ sm64gs2pc-core/src/lib.rs
@@ -4,9 +4,4 @@ pub mod gameshark;
 mod left_value;
 mod typ;
 
-pub use decl::Decl;
-pub use decl::DeclKind;
 pub use decomp_data::DecompData;
-pub use typ::Struct;
-pub use typ::StructField;
-pub use typ::Type;
