bf207a2c0577988403f0a75ded81246ea1b34320
Benjamin Levy <blevy@protonmail.com>
2020-08-25 21:18:17 -0400

    Download decomp data from assets repo instead of building it

--- sm64gs2pc/build.rs
+++ sm64gs2pc/build.rs
@@ -1,17 +1,22 @@
 use std::env;
 use std::fs::File;
-use std::io::BufWriter;
+use std::io::Write;
 use std::path::Path;
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
-    let decomp_data = sm64gs2pc_core::DecompData::load(
-        &Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("baserom.us.z64"),
-        &tempfile::TempDir::new().unwrap().path().join("sm64"),
-    );
-
     let path = Path::new(&env::var("OUT_DIR").unwrap()).join("decomp_data.bincode");
 
-    bincode::serialize_into(BufWriter::new(File::create(path).unwrap()), &decomp_data).unwrap();
+    let bytes = async {
+        reqwest::get("https://github.com/sm64gs2pc/assets/raw/master/decomp_data.bincode")
+            .await
+            .unwrap()
+            .bytes()
+            .await
+            .unwrap()
+    };
+    let bytes = tokio::runtime::Runtime::new().unwrap().block_on(bytes);
+
+    File::create(path).unwrap().write_all(&*bytes).unwrap();
 }
